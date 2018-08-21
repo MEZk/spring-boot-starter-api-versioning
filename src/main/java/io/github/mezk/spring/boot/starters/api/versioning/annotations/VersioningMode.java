0b96b3e8dfd6dca7f1f22a121a19dd80146eb768
Andrei Selkin <andreyselkin@gmail.com>
2018-08-21 22:58:23 +0300

    Issue #14: Add javadoc comments for all methods and classes

--- src/main/java/io/github/mezk/spring/boot/starters/api/versioning/annotations/VersioningMode.java
+++ src/main/java/io/github/mezk/spring/boot/starters/api/versioning/annotations/VersioningMode.java
@@ -1,5 +1,15 @@
 package io.github.mezk.spring.boot.starters.api.versioning.annotations;
 
+/**
+ * The constatnts of this enum set the api versioning mode.
+ * The current implementation supports only {@link VersioningMode#ANNOTATION} mode, which allows
+ * to use {@link ApiVersion} annotation to specify the api version.
+ *
+ * @author Andrei Selkin
+ */
 public enum VersioningMode {
+    /**
+     * Allows to specify api version via {@link ApiVersion} annotation.
+     */
     ANNOTATION
 }
--- src/main/java/io/github/mezk/spring/boot/starters/api/versioning/configuration/ApiVersioningProperties.java
+++ src/main/java/io/github/mezk/spring/boot/starters/api/versioning/configuration/ApiVersioningProperties.java
@@ -2,24 +2,51 @@ package io.github.mezk.spring.boot.starters.api.versioning.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * The following class contains api versioning configuration properties.
+ *
+ * @author Andrei Selkin
+ */
 @ConfigurationProperties("api.versioning")
 public class ApiVersioningProperties {
 
     private String apiPathVersionPrefix;
     private String defaultVersion;
 
+    /**
+     * Returns api path version prefix.
+     * This prefix will be inserted before api version.
+     * For example, if the value of the property is set to 'v' and the api version is set to '1',
+     * the final path will be '/v1'.
+     * @return api path version prefix.
+     */
     public String getApiPathVersionPrefix() {
         return apiPathVersionPrefix;
     }
 
+    /**
+     * Sets api path version prefix.
+     * This prefix will be inserted before api version.
+     * For example, if the value of the property is set to 'v' and the api version is set to '1',
+     * the final path will be '/v1'.
+     * @param apiPathVersionPrefix api version prefix value.
+     */
     public void setApiPathVersionPrefix(String apiPathVersionPrefix) {
         this.apiPathVersionPrefix = apiPathVersionPrefix;
     }
 
+    /**
+     * Returns default api version.
+     * @return default api version.
+     */
     public String getDefaultVersion() {
         return defaultVersion;
     }
 
+    /**
+     * Sets default api version.
+     * @param defaultVersion defalt api version.
+     */
     public void setDefaultVersion(String defaultVersion) {
         this.defaultVersion = defaultVersion;
     }
